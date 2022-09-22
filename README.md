e5be35efc7c25277d9f7fb4c5fb98495d8cf31f3
Matheus Santana <matheus.santana6415@gmail.com>
2022-09-22 17:14:18 -0300

    Comentários e mudança de cor dos objetos

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ProjetoPG
 Projeto para a matéria de Processamento Gráfico da UFSCar.
+
+- Objetivo: Criar e visualizar uma cena 3D, mapeando os conceitos estudados.
+
+Implementação: JavaScript + WebGL (sem utilizar bibliotecas 3D, exceto pequenas bibliotecas de auxílio)
+
+Especificações/Avaliação:
+- Visualização de pelo menos um objeto 3D por membro do grupo, redimensionando e posicionando cada objeto individualmente no ambiente virtual;
+- Utilização de pelo menos dois shaders (vertex/fragment);
+- Definição de pelo menos uma câmera;
+- Movimento simples de pelo menos um objeto;
+- Documentar no github (readme principal) as especificações do projeto, modo de interação e principais características implementadas.
--- public/main.js
+++ public/main.js
@@ -1,12 +1,14 @@
+//Referencia pro canvas
 const canvas = document.querySelector('canvas');
+//Contexto WebGL do canvas
 const gl = canvas.getContext('webgl');
 
+//Verifica se navegador suporta WebGL
 if (!gl) {
     throw new Error('WebGL not supported');
 }
 
-
-
+//Cria Vertex Shader
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
 precision mediump float;
@@ -19,8 +21,10 @@ void main() {
     gl_Position = matrix * vec4(position, 1);
 }
 `);
+
 gl.compileShader(vertexShader);
 
+//Cria Fragment Shader
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
 precision mediump float;
@@ -30,10 +34,10 @@ void main() {
 }
 `);
 
-
 gl.compileShader(fragmentShader);
 console.log(gl.getShaderInfoLog(fragmentShader));
 
+//Cria o programa e anexa os shaders ao programa
 const program = gl.createProgram();
 gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
@@ -44,13 +48,11 @@ gl.useProgram(program);
 gl.enable(gl.DEPTH_TEST);
 
 
-
 const uniformLocations = {
     matrix: gl.getUniformLocation(program, `matrix`),
 };
 
-
-
+//Carrega objetos (sphere.obj)
 function  loadFileAJAX(name) {
     var xhr = new XMLHttpRequest(),
     okStatus = document.location.protocol === "file:" ? 0 : 200;
@@ -61,23 +63,24 @@ function  loadFileAJAX(name) {
 
 const obj =  parseOBJ(loadFileAJAX('models/sphere.obj')).geometries[0].data;
 
-
 const vertexData = obj.position
 
 const earthColor= []
 const moonColor = []
 
+//Define as cores de cada vertice dos objetos
 for (var i = 0; i < obj.position.length; i++){
-    earthColor.push(0)
-    earthColor.push(0)
-    earthColor.push(Math.random())
-
-    let a = Math.random()
-    moonColor.push(a)
-    moonColor.push(a)
-    moonColor.push(a)
-
+    //Cores da Terra
+    earthColor.push(0)  //R
+    earthColor.push(.2) //G
+    earthColor.push(.9) //B
+    
+    //Cores da Lua
+    moonColor.push(0.6) //R
+    moonColor.push(0.6) //G
+    moonColor.push(0.6) //B
 }
+
 
 var objectsToDraw = [
     {
@@ -105,21 +108,22 @@ const moonMatrix = mat4.create();
 const viewMatrix = mat4.create();
 const projectionMatrix = mat4.create();
 mat4.perspective(projectionMatrix, 
-    75 * Math.PI/180, // vertical field-of-view (angle, radians)
-    canvas.width/canvas.height, // aspect W/H
-    1e-4, // near cull distance
-    1e4 // far cull distance
+    75 * Math.PI/180, // Campo-de-visão vertical (angle, radians)
+    canvas.width/canvas.height, // Aspecto Largura/Altura (W/H)
+    1e-4, // Distancia de corte proxima
+    1e4 // Distancia de corte distante
 );
 
 const mvMatrix = mat4.create();
 const mvpMatrix = mat4.create();
 
+//Posicao e tamanho da Terra
 mat4.translate(earthMatrix, earthMatrix, [0, 0, -5]);
 mat4.scale(earthMatrix, earthMatrix, [30, 30, 30])
 
-mat4.translate(moonMatrix, moonMatrix, [0, 0, -5]);
+//Posicao e tamanho da Lua
+mat4.translate(moonMatrix, moonMatrix, [0, 0, -10]);
 mat4.scale(moonMatrix, moonMatrix, [10, 10, 10])
-
 
 mat4.translate(viewMatrix, viewMatrix, [0, 0, 10]);
 mat4.invert(viewMatrix, viewMatrix);
@@ -127,17 +131,19 @@ mat4.invert(viewMatrix, viewMatrix);
 var currentTime = Date.now();
 var animationStartTime = currentTime;
 
+//Funcao de animacao para mover a lua e girar a terra
 function animate() {
-    currentTime = Date.now();
-    
-    
+
+    //Chama a animacao novamente
     requestAnimationFrame(animate);
+
     objectsToDraw.forEach( e => {
-        
+        //Buffer da posicao de cada objeto
         const positionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(e.position), gl.STATIC_DRAW);
 
+        //Buffer da cor de cada objeto
         const colorBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(e.color), gl.STATIC_DRAW);
@@ -152,20 +158,24 @@ function animate() {
         gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
         gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
+        //Obtem horario atual
+        currentTime = Date.now();
 
+        //Rotacao da Terra
         if(e.name == 'earth'){
             mat4.rotateY(earthMatrix, earthMatrix, Math.PI/2 /100);
             mat4.multiply(mvMatrix, viewMatrix, earthMatrix);
         }
 
+        //Movimentacao da Lua
         if(e.name == 'moon'){
-            let orbitalMultipler = 5000
-            let circleRadius = 0.02
-            let angle = (currentTime - animationStartTime)/orbitalMultipler*2*Math.PI % 
-            (2*Math.PI);
+            let orbitalMultipler = 5000;
+            let circleRadius = 0.005;
+            let elapsedTime = currentTime - animationStartTime;
+            let angle = elapsedTime / orbitalMultipler*2*Math.PI % (2*Math.PI);
             let x = Math.cos(angle) * circleRadius;
             let z = Math.sin(angle) * circleRadius;
-            
+
             mat4.translate(moonMatrix,moonMatrix, [x, 0, z]),
             //mat4.scale(moonMatrix, moonMatrix, [0.5, 0.5, 0.5]);
             mat4.multiply(mvMatrix, viewMatrix, moonMatrix);
@@ -175,9 +185,7 @@ function animate() {
         gl.uniformMatrix4fv(e.uniforms.matrix, false, mvpMatrix);
         gl.drawArrays(gl.TRIANGLES, 0, e.position.length / 3);
     })
-
-    
-
 }
 
-animate();
+//Executa funcao de animacao
+requestAnimationFrame(animate);
