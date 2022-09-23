7fbacbbd2b59725a6704fb19739fd85b50292828
LucasSsouza10 <luccas.ls63@gmail.com>
2022-09-22 23:44:58 -0300

    limpeza codigo + add velocidades de rotação lua e terra e velocidade revolução

--- public/main.js
+++ public/main.js
@@ -31,97 +31,38 @@ const vertexData = obj.position
 const earthColor= []
 const moonColor = []
 
+
 //Define as cores de cada vertice dos objetos
-for (var i = 0; i < obj.position.length; i++){
+for (var i = 0; i < obj.position.length/3; i++){
     //Cores da Terra
     earthColor.push(0)  //R
-    earthColor.push(.2) //G
+    earthColor.push(Math.random() % .5) //G
     earthColor.push(.9) //B
     
     //Cores da Lua
+    
     moonColor.push(0.6) //R
     moonColor.push(0.6) //G
     moonColor.push(0.6) //B
 }
 
 
-// var objectsToDraw = [
-//     {
-//       name: 'earth',
-//       programInfo: program,
-
-//       uniforms: uniformLocations,
-//       position: vertexData,
-//       color: earthColor,
-//     },
-
-//     {
-//         name: 'moon',
-//         programInfo: program,
-  
-//         uniforms: uniformLocations,
-//         position: vertexData,
-//         color: moonColor,
-//       }
-//   ];
-
-
-
-var earthNode = new Node();
+var earthNode = new Node(program, vertexData, earthColor, uniformLocations);
 mat4.translate(earthNode.localMatrix, earthNode.localMatrix, [0, 0, 0]);
-earthNode.drawInfo = {
-    name: 'earth',
-    uniforms: uniformLocations,
-    programInfo: program,
-    position: vertexData,
-    color : earthColor
-};
 
-var moonNode = new Node();
+
+var moonNode = new Node(program, vertexData, moonColor, uniformLocations);
 mat4.translate(moonNode.localMatrix, moonNode.localMatrix, [.1, 0, 0]);  // moon .1 units from the earth
-mat4.scale(moonNode.localMatrix, moonNode.localMatrix, [.3, .3, .3]);  // moon .1 units from the earth
+mat4.scale(moonNode.localMatrix, moonNode.localMatrix, [.3, .3, .3]);  // 
 
-moonNode.drawInfo = {
-    name: 'moon',
-    uniforms: uniformLocations,
-    programInfo: program,
-    position: vertexData,
-    color: moonColor,
-};
-
- // connect the celetial objects
- moonNode.setParent(earthNode);
+// seta lua como nó filho da terra 
+moonNode.setParent(earthNode);
 
 var objects = [
     earthNode,
     moonNode,
 ];
 
-
-const earthMatrix = mat4.create();
-const moonMatrix = mat4.create();
-
-//viewMatrix
-const viewMatrix = mat4.create();
-const projectionMatrix = mat4.create();
-mat4.perspective(projectionMatrix, 
-    75 * Math.PI/180, // Campo-de-visão vertical (angle, radians)
-    canvas.width/canvas.height, // Aspecto Largura/Altura (W/H)
-    1e-4, // Distancia de corte proxima
-    1e4 // Distancia de corte distante
-);
-const mvMatrix = mat4.create();
-const mvpMatrix = mat4.create();
-
-//Posicao e tamanho da Terra
-mat4.scale(earthMatrix, earthMatrix, [30, 30, 30]);
-
-//Posicao e tamanho da Lua
-mat4.scale(moonMatrix, moonMatrix, [10, 10, 10]);
-mat4.translate(moonMatrix, moonMatrix, [0, 0, 0]); //PRECISA AJUSTAR!!!
-
-//Camera
-mat4.translate(viewMatrix, viewMatrix, [0, 0, -10]);
 
 //Controlar Camera
 window.onkeypress = e => {
@@ -169,77 +110,7 @@ window.onkeypress = e => {
     }
 }
 
-var currentTime = Date.now();
-var animationStartTime = currentTime;
-
-//Funcao de animacao para mover a lua e girar a terra
-function animate() {
-    var currentTime = Date.now();
-    // Update FPS if a second or more has passed since last FPS update
-    if(currentTime - gl.previousFrameTimeStamp >= 1000) {
-        gl.fpsCounter.innerHTML = pwgl.nbrOfFramesForFPS;
-        gl.nbrOfFramesForFPS = 0;
-        gl.previousFrameTimeStamp = currentTime;
-    }
-
-    //Chama a animacao novamente
-    requestAnimationFrame(animate);
-
-    objectsToDraw.forEach( e => {
-        gl.useProgram(e.programInfo,); 
-        
-        //Buffer da posicao de cada objeto
-        const positionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(e.position), gl.STATIC_DRAW);
-
-        //Buffer da cor de cada objeto
-        const colorBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(e.color), gl.STATIC_DRAW);
-
-        const positionLocation = gl.getAttribLocation(e.programInfo, `position`);
-        gl.enableVertexAttribArray(positionLocation);
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
-
-        const colorLocation = gl.getAttribLocation(e.programInfo, `color`);
-        gl.enableVertexAttribArray(colorLocation);
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
-
-        //Obtem horario atual
-        currentTime = Date.now();
-
-        //Rotacao da Terra
-        if(e.name == 'earth'){
-            //mat4.rotateY(earthMatrix, earthMatrix, Math.PI/2 /100);
-            mat4.multiply(mvMatrix, viewMatrix, earthMatrix);
-        }
-
-        //Movimentacao da Lua
-        if(e.name == 'moon'){
-            let orbitalMultipler = 2000;
-            let circleRadius = .05;
-            let elapsedTime = currentTime - animationStartTime;
-            let angle = elapsedTime / orbitalMultipler*2*Math.PI % (2*Math.PI);
-            let x = Math.cos(angle) * circleRadius;
-            let z = Math.sin(angle) * circleRadius;
-
-            mat4.translate(moonMatrix,moonMatrix, [x, 0, z]),
-            mat4.multiply(mvMatrix, viewMatrix, moonMatrix);
-        }
-        
-        mat4.multiply(mvpMatrix, projectionMatrix, mvMatrix);
-        gl.uniformMatrix4fv(e.uniforms.matrix, false, mvpMatrix);
-        gl.drawArrays(gl.TRIANGLES, 0, e.position.length / 3);
-
-        
-    })
-}
-
-//Executa funcao de animacao
-//requestAnimationFrame(animate);
+var baseSpeed = .01
 
 requestAnimationFrame(drawScene);
 // Draw the scene.
@@ -270,37 +141,36 @@ function drawScene() {
 
     var viewProjectionMatrix = mat4.create();
     mat4.multiply(viewProjectionMatrix,projectionMatrix, viewMatrix);
+    
+    let earthRotationSpeed = baseSpeed; // velocidade de rotação da terra
+    let revolutionSpeed = earthRotationSpeed * 2.2 // velocidade de revolução da lua é 2.2 vezes rotação da terra
+    let moonRotationSpeed = earthRotationSpeed/27 
 
-    // upda6
-    mat4.rotateY(earthNode.localMatrix, earthNode.localMatrix, .07);
-    mat4.rotateY(moonNode.localMatrix, moonNode.localMatrix, .5);
+    // atualiza as matrizes de cada objeto
+    mat4.rotateY(earthNode.localMatrix, earthNode.localMatrix, revolutionSpeed); 
+    mat4.rotateY(moonNode.localMatrix, moonNode.localMatrix, moonRotationSpeed);
+
+    // atualiza aplica operação na matriz de um objeto de somente
+    mat4.rotateY(earthNode.privateMatrix, earthNode.privateMatrix, earthRotationSpeed - revolutionSpeed); // reduz a velocidade de toração da terra
 
     // Update all world matrices in the scene graph
     earthNode.updateWorldMatrix();
+
+
 
         
 
     objects.forEach( e => {
         gl.useProgram(  e.drawInfo.programInfo,); 
         
-        //Buffer da posicao de cada objeto
-        const positionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(e.drawInfo.position), gl.STATIC_DRAW);
-
-        //Buffer da cor de cada objeto
-        const colorBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(e.drawInfo.color), gl.STATIC_DRAW);
-
         const positionLocation = gl.getAttribLocation(e.drawInfo.programInfo, `position`);
         gl.enableVertexAttribArray(positionLocation);
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, e.buffers.positionBuffer);
         gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
         const colorLocation = gl.getAttribLocation(e.drawInfo.programInfo, `color`);
         gl.enableVertexAttribArray(colorLocation);
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, e.buffers.colorBuffer);
         gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
         var mvpMatrix = mat4.create();
--- public/node.js
+++ public/node.js
@@ -1,7 +1,15 @@
-var Node = function() {
+var Node = function(program, position, color, uniforms) {
     this.children = [];
     this.localMatrix = mat4.create();
+    this.privateMatrix = mat4.create();
     this.worldMatrix = mat4.create();
+    this.drawInfo = {
+        programInfo: program,
+        position: position,
+        color: color,
+        uniforms: uniforms,
+    };
+    this.startBuffer()
   };
   
 Node.prototype.setParent = function(parent) {
@@ -23,7 +31,7 @@ this.parent = parent;
 Node.prototype.updateWorldMatrix = function(matrix) {
     if (matrix) {
         // a matrix was passed in so do the math
-        mat4.multiply(this.worldMatrix, matrix, this.localMatrix, );
+        mat4.multiply(this.worldMatrix, matrix, this.localMatrix);
     } else {
         // no matrix was passed in so just copy.
         mat4.copy(this.worldMatrix, this.localMatrix);
@@ -34,4 +42,25 @@ Node.prototype.updateWorldMatrix = function(matrix) {
     this.children.forEach(function(child) {
         child.updateWorldMatrix(worldMatrix);
     });
+
+    mat4.multiply(this.worldMatrix, this.privateMatrix, this.worldMatrix );
 }
+
+Node.prototype.startBuffer = function() {
+    //Buffer da posicao de cada objeto
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.drawInfo.position), gl.STATIC_DRAW);
+
+    //Buffer da cor de cada objeto
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.drawInfo.color), gl.STATIC_DRAW);
+    
+
+    this.buffers = { 
+        positionBuffer: positionBuffer,
+        colorBuffer: colorBuffer,
+    }
+}
+    
